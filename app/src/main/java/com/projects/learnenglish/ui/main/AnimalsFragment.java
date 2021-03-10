1cb7cc5a83ca16c1d619dc097a650a322d62b85b
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-10 16:25:59 -0300

    Onclick and sounds logic added

--- app/src/main/java/com/projects/learnenglish/ui/main/AnimalsFragment.java
+++ app/src/main/java/com/projects/learnenglish/ui/main/AnimalsFragment.java
@@ -1,5 +1,6 @@
 package com.projects.learnenglish.ui.main;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.projects.learnenglish.R;
 
@@ -15,7 +17,7 @@ import com.projects.learnenglish.R;
  * Use the {@link AnimalsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AnimalsFragment extends Fragment {
+public class AnimalsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +27,13 @@ public class AnimalsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageView cao;
+    private ImageView gato;
+    private ImageView leao;
+    private ImageView macaco;
+    private ImageView ovelha;
+    private ImageView vaca;
+    private MediaPlayer mediaPlayer;
 
     public AnimalsFragment() {
         // Required empty public constructor
@@ -61,6 +70,68 @@ public class AnimalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animals, container, false);
+        View view =  inflater.inflate(R.layout.fragment_animals, container, false);
+        cao = view.findViewById(R.id.cao);
+        gato = view.findViewById(R.id.gato);
+        leao = view.findViewById(R.id.leao);
+        macaco = view.findViewById(R.id.macaco);
+        ovelha = view.findViewById(R.id.ovelha);
+        vaca = view.findViewById(R.id.vaca);
+        cao.setOnClickListener(this);
+        gato.setOnClickListener(this);
+        leao.setOnClickListener(this);
+        macaco.setOnClickListener(this);
+        ovelha.setOnClickListener(this);
+        vaca.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.cao):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.dog);
+                playSound();
+                break;
+            case (R.id.gato):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cat);
+                playSound();
+                break;
+            case (R.id.leao):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.lion);
+                playSound();
+                break;
+            case (R.id.macaco):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.monkey);
+                playSound();
+                break;
+            case (R.id.ovelha):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sheep);
+                playSound();
+                break;
+            case (R.id.vaca):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cow);
+                playSound();
+                break;
+        }
+    }
+    public void playSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/projects/learnenglish/ui/main/NumbersFragment.java
+++ app/src/main/java/com/projects/learnenglish/ui/main/NumbersFragment.java
@@ -1,12 +1,15 @@
 package com.projects.learnenglish.ui.main;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.projects.learnenglish.R;
 
@@ -15,7 +18,7 @@ import com.projects.learnenglish.R;
  * Use the {@link NumbersFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NumbersFragment extends Fragment {
+public class NumbersFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +28,13 @@ public class NumbersFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageView um;
+    private ImageView dois;
+    private ImageView tres;
+    private ImageView quatro;
+    private ImageView cinco;
+    private ImageView seis;
+    private MediaPlayer mediaPlayer;
 
     public NumbersFragment() {
         // Required empty public constructor
@@ -61,6 +71,69 @@ public class NumbersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numbers, container, false);
+        View view = inflater.inflate(R.layout.fragment_numbers, container, false);
+        um = view.findViewById(R.id.um);
+        dois = view.findViewById(R.id.dois);
+        tres = view.findViewById(R.id.tres);
+        quatro = view.findViewById(R.id.quatro);
+        cinco = view.findViewById(R.id.cinco);
+        seis = view.findViewById(R.id.seis);
+        um.setOnClickListener(this);
+        dois.setOnClickListener(this);
+        tres.setOnClickListener(this);
+        quatro.setOnClickListener(this);
+        cinco.setOnClickListener(this);
+        seis.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.um):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+                playSound();
+                break;
+            case (R.id.dois):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+                playSound();
+                break;
+            case (R.id.tres):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+                playSound();
+                break;
+            case (R.id.quatro):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+                playSound();
+                break;
+            case (R.id.cinco):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+                playSound();
+                break;
+            case (R.id.seis):
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+                playSound();
+                break;
+        }
+    }
+    public void playSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
